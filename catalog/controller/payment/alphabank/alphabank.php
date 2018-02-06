f1ab2e352ade11cdbc01d06ccd8c46c75e074e63
atemirov <aztemirov@gmail.com>
2018-02-07 00:07:00 +0300

    Update alphabank.php

--- catalog/controller/payment/alphabank/alphabank.php
+++ catalog/controller/payment/alphabank/alphabank.php
@@ -7,50 +7,53 @@
 //====================================================
 class ControllerPaymentAlphabank extends Controller{
     public function index() {
+		$this->load->model('checkout/order');
         $this->load->language('payment/alphabank');
-	$data['button_confirm'] = $this->language->get('button_confirm');
-	$data['action'] = $this->url->link('payment/alphabank/checkout', '', 'SSL');
+		$data['button_confirm'] = $this->language->get('button_confirm');
+		$data['action'] = $this->url->link('payment/alphabank/checkout', '', 'SSL');
         $data['payment_desc']=  $this->language->get('text_description');
-	if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/payment/alphabank.tpl')) {
-            return $this->load->view($this->config->get('config_template') . '/template/payment/alphabank.tpl', $data);
-	} else {
-            return $this->load->view('default/template/payment/alphabank.tpl', $data);
-	}
+
+		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/payment/alphabank.tpl')) {
+				return $this->load->view($this->config->get('config_template') . '/template/payment/alphabank.tpl', $data);
+		} else {
+				return $this->load->view('default/template/payment/alphabank.tpl', $data);
+		}
     }
     
     public function checkout() {
         $this->load->model('checkout/order');
-	$this->load->model('account/order');
-	$this->load->model('payment/alphabank');
+		$this->load->model('account/order');
+		$this->load->model('payment/alphabank');
         
         $order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
         $this->load->model('extension/extension');
         $results = $this->model_extension_extension->getExtensions('total');
-	$order_data = array();
-	$total = 0;
-	$items = array();
-	$taxes = $this->cart->getTaxes();
+		$order_data = array();
+		$total = 0;
+		$items = array();
+		$taxes = $this->cart->getTaxes();
         
         $i = 0;
-	foreach ($results as $result) {
-            if ($this->config->get($result['code'] . '_status')) {
-		$this->load->model('total/' . $result['code']);
-		$this->{'model_total_' . $result['code']}->getTotal($order_data['totals'], $total, $taxes);
-		if (isset($order_data['totals'][$i])) {
-                    if (strstr(strtolower($order_data['totals'][$i]['code']), 'total') === false) {
-			$item = new stdClass();
-			$item->sku = $order_data['totals'][$i]['code'];
-			$item->name = $order_data['totals'][$i]['title'];
-			$item->amount = $order_data['totals'][$i]['value'];
-			$item->qty = 1;
-			$items[] = $item;
-                    }
-                    $i++;
+		foreach ($results as $result) {
+			if ($this->config->get($result['code'] . '_status')) {
+				$this->load->model('total/' . $result['code']);
+				$this->{'model_total_' . $result['code']}->getTotal($order_data['totals'], $total, $taxes);
+				if (isset($order_data['totals'][$i])) {
+					if (strstr(strtolower($order_data['totals'][$i]['code']), 'total') === false) {
+						$item = new stdClass();
+						$item->sku = $order_data['totals'][$i]['code'];
+						$item->name = $order_data['totals'][$i]['title'];
+						$item->amount = $order_data['totals'][$i]['value'];
+						$item->qty = 1;
+						$items[] = $item;
+					}
+					$i++;
+				}
+			}
 		}
-            }
-	}
         
         $ordered_products = $this->model_account_order->getOrderProducts($this->session->data['order_id']);
+		
         foreach ($ordered_products as $product) {
             $item = new stdClass();
             $item->sku = $product['product_id'];
@@ -58,12 +61,12 @@ class ControllerPaymentAlphabank extends Controller{
             $item->amount = $product['price'] * $product['quantity'];
             $item->qty = $product['quantity'];
             $items[] = $item;
-	}
+		}
         
 
         $_sgateway = ($this->config->get('alphabank_test'))?'https://web.rbsuat.com/ab/rest/':'https://engine.paymentgate.ru/payment/rest/';
-        $_slogin = $this->config->get('alphabank_payment_slogin');
-        $_spassword = $this->config->get('alphabank_payment_spassword');
+        $_slogin = $this->config->get('alphabank_slogin');
+        $_spassword = $this->config->get('alphabank_spassword');
         $_totalcop = $order_info['total']*100;
         $_totalrub = $order_info['total'];
         
@@ -73,8 +76,8 @@ class ControllerPaymentAlphabank extends Controller{
             'orderNumber' => urlencode($this->session->data['order_id']),
             'amount' => urlencode($_totalcop),
             'returnUrl' => $this->url->link('payment/alphabank/result')
-	);
-        
+		);
+
         $_responce = $this->model_payment_alphabank->gateway($_sgateway, 'register.do', $_qdata);
         if (isset($_responce['errorCode'])) {
             unset($_SESSION['order_id']);
@@ -102,8 +105,8 @@ class ControllerPaymentAlphabank extends Controller{
             $this->load->language('payment/alphabank');
         
             $_sgateway = ($this->config->get('alphabank_test'))?'https://web.rbsuat.com/ab/rest/':'https://engine.paymentgate.ru/payment/rest/';
-            $_slogin = $this->config->get('alphabank_payment_slogin');
-            $_spassword = $this->config->get('alphabank_payment_spassword');
+            $_slogin = $this->config->get('alphabank_slogin');
+            $_spassword = $this->config->get('alphabank_spassword');
             $_qdata = array(
                 'userName' => $_slogin,
                 'password' => $_spassword,
@@ -132,7 +135,7 @@ class ControllerPaymentAlphabank extends Controller{
             $this->cart->clear(); // Очистить корзину
             unset($_SESSION['order_id']);
             $this->response->redirect($this->url->link('checkout/success', 'token=' . $this->session->data['token'], 'SSL'));
-	}else{
+		}else{
             unset($_SESSION['order_id']);
             $this->session->data['error'] = 'Неверный статус проведения платежа';
             $this->response->redirect($this->url->link('checkout/cart'));
